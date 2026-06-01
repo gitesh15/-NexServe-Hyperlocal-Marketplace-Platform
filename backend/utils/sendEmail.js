ae75fa169e8397644276d9a5f15f277dc90abd9c
Gitesh Thorat <giteshthorat15@gmail.com>
2026-06-01 13:25:27 +0530

    15-1 update

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,46 +1,30 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-
-  port: 587,
-
-  secure: false,
-
-  auth: {
-    user: process.env.EMAIL_USER,
-
-    pass: process.env.EMAIL_PASS,
-  },
-
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (email, otp) => {
   try {
-    const info = await transporter.sendMail({
-      from: `"NexServe" <${process.env.EMAIL_USER}>`,
+    const response = await resend.emails.send({
+      from: "NexServe <onboarding@resend.dev>",
 
       to: email,
 
       subject: "NexServe Email Verification OTP",
 
       html: `
-        <div style="font-family:Arial;padding:20px">
+        <div style="font-family:sans-serif;padding:20px">
           <h2>NexServe Verification</h2>
 
           <p>Your OTP is:</p>
 
           <h1>${otp}</h1>
 
-          <p>This OTP is valid for 5 minutes.</p>
+          <p>Valid for 5 minutes.</p>
         </div>
       `,
     });
 
-    console.log("EMAIL SENT:", info.response);
+    console.log("EMAIL SENT:", response);
   } catch (error) {
     console.log("EMAIL ERROR:", error);
 
